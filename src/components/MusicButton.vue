41c8c4052fb62e0877480a8359649e309502047c
Rogel Gerodiaz <gerodiazrogel0@gmail.com>
2025-04-22 13:31:05 +0800

    initial ui

---
+++ src/components/MusicButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="music-btn-container">
+    <UnMute class="music-btn" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import UnMute from '@/assets/svg/unmute_svg.vue'
+</script>
+
+<style scoped>
+.music-btn {
+  position: absolute;
+  right: 10%;
+  top: 3%;
+}
+
+.music-btn:hover {
+  scale: 1.1;
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="home-page">
+    <MusicBtn />
     <div class="title-container">
       <h1 class="title">Ace+</h1>
     </div>
@@ -26,6 +27,7 @@ import { ref } from 'vue'
 import { usePlayerStore } from '@/stores/playerStore'
 import { useRouter } from 'vue-router'
 import AuthModal from '@/components/AuthModal.vue'
+import MusicBtn from '@/components/MusicButton.vue'
 
 import { showNotify } from '@/utils/notify'
 
