2a874492e0694d28c750c9a2542c16602aca525b
PLUS\Fernando.Acebuche <fernando.acebuche@tbu.net>
2025-04-24 08:33:03 +0800

    fixed balance decimal count

--- src/utils/convertMoney.ts
+++ src/utils/convertMoney.ts
@@ -1,15 +1,15 @@
 export const convertToReadableFormat = (value: number) => {
   if (value >= 1000000000) {
-    return (value / 1000000000).toFixed(1) + 'B' // Convert to billions
+    return (value / 1000000000).toFixed(1) + 'B'
   } else if (value >= 1000000) {
-    return (value / 1000000).toFixed(value % 1000000 === 0 ? 0 : 1) + 'M' // Convert to millions with flexible decimals
+    return (value / 1000000).toFixed(value % 1000000 === 0 ? 0 : 1) + 'M'
   } else if (value >= 1000) {
-    return (value / 1000).toFixed(value % 1000 === 0 ? 0 : 1) + 'k' // Convert to thousands with flexible decimals
+    return (value / 1000).toFixed(value % 1000 === 0 ? 0 : 1) + 'k'
   } else {
-    return value.toString() // If less than 1000, return the original number
+    return Number(value.toFixed(1)).toLocaleString('en-US', { minimumFractionDigits: 1 })
   }
 }
 
 export const formatCurrency = (value: number) => {
-  return value.toLocaleString('en-US') // Formats as "100,000"
+  return Number(value.toFixed(2)).toLocaleString('en-US', { minimumFractionDigits: 2 })
 }
