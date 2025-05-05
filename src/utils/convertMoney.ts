85da9b33daf43c1480aa0d11d83d1fbbcd12c2a9
Jose Acebuche <joseacebuche2@gmail.com>
2025-05-05 23:29:59 +0800

    fixed convertToReadableFormat function

--- src/utils/convertMoney.ts
+++ src/utils/convertMoney.ts
@@ -1,31 +1,25 @@
-export const convertToReadableFormat = (value: number) => {
-  const hasDecimal = value % 1 !== 0;
-
+export const convertToReadableFormat = (value: number): string => {
+  const format = (num: number, suffix: string, forceDecimal = false) => {
+    const hasDecimal = forceDecimal || num % 1 !== 0;
+    return (
+      num.toLocaleString('en-US', {
+        minimumFractionDigits: hasDecimal ? 1 : 0,
+        maximumFractionDigits: hasDecimal ? 1 : 0,
+      }) + suffix
+    );
+  };
   if (value >= 1_000_000_000) {
-    return (
-      (value / 1_000_000_000).toLocaleString('en-US', {
-        minimumFractionDigits: hasDecimal ? 1 : 0,
-        maximumFractionDigits: hasDecimal ? 1 : 0,
-      }) + 'B'
-    );
+    return format(value / 1_000_000_000, 'B');
   } else if (value >= 1_000_000) {
-    return (
-      (value / 1_000_000).toLocaleString('en-US', {
-        minimumFractionDigits: hasDecimal ? 1 : 0,
-        maximumFractionDigits: hasDecimal ? 1 : 0,
-      }) + 'M'
-    );
+    return format(value / 1_000_000, 'M');
+  } else if (value >= 100_000) {
+    return format(value / 1_000, 'K');
   } else if (value >= 1_000) {
-    return (
-      (value / 1_000).toLocaleString('en-US', {
-        minimumFractionDigits: hasDecimal ? 1 : 0,
-        maximumFractionDigits: hasDecimal ? 1 : 0,
-      }) + 'k'
-    );
+    return format(value / 1_000, 'k', true);
   } else {
     return value.toLocaleString('en-US', {
-      minimumFractionDigits: hasDecimal ? 1 : 0,
-      maximumFractionDigits: hasDecimal ? 1 : 0,
+      minimumFractionDigits: value % 1 !== 0 ? 2 : 0,
+      maximumFractionDigits: value % 1 !== 0 ? 2 : 0,
     });
   }
 };
